1b6b49d0ab8673a2aa857087c2195f7e148b38d3
Naoko <daisuki_tarebuta@yahoo.co.jp>
2023-05-10 07:42:49 +0900

    更新

--- components/GitHub.js
+++ components/GitHub.js
@@ -2,15 +2,18 @@ import { Link } from '@chakra-ui/react'
 import CustomIcon from './CustomIcon'
 
 const Github = () => {
-    return (
-      <>
-        <Link
-          href="https://github.com/kurobo-chan/nextjs-weather-app"
-          isExternal
-        >
-          <CustomIcon icon="github" boxSize={6} />
-        </Link>
-      </>
-    )
+  return (
+    <>
+      <Link
+        href="https://github.com/kurobo-chan/nextjs-weather-app"
+        isExternal
+        w={`2.5em`}
+        h={`2.5em`}
+        style={{ display: 'grid', placeContent: 'center' }}
+      >
+        <CustomIcon icon="github" boxSize={6} />
+      </Link>
+    </>
+  )
 }
 export default Github
